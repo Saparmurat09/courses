810aa7c26b701709a2a1a08202a999c3a6cf5ba3
Saparmurat <saparmurat@protonmail.com>
2022-10-17 22:02:49 +0600

    register models

--- coursesapp/admin.py
+++ coursesapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Contact, Category, Branch, Course
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    model = Contact
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    model = Branch
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    model = Course
