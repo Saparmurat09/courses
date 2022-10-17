a39b7ed3cc9b36896bf1afc9cfa6142ef6983ddd
Saparmurat <saparmurat@protonmail.com>
2022-10-17 23:41:54 +0600

    add serializers

---
+++ coursesapp/serializers.py
@@ -0,0 +1,26 @@
+from .models import Course, Contact, Category, Branch
+
+from rest_framework import serializers
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ['type_of', 'value']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name', 'imgpath']
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = ['latitude', 'longitude', 'address']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['name', 'description', 'category', 'logo', 'contacts', 'branches']
