04c90998c40ef8f7d88b4356308efd0528598740
Saparmurat <saparmurat@protonmail.com>
2022-10-19 23:34:21 +0600

    add swagger, redoc

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Courses API",
+      default_version='v0.1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="some.email@gmail.com"),
+      license=openapi.License(name="No License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('coursesapp.urls'))
+    path('', include('coursesapp.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^api(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^api/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
