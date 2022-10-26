c08e83c8511365468b9ead5b7d85ae32cb074395
Saparmurat <saparmurat@protonmail.com>
2022-10-26 20:57:36 +0600

    specify names for urls

--- coursesapp/urls.py
+++ coursesapp/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('courses/', views.CourseList.as_view()),
-    path('courses/<int:pk>/', views.CourseDetail.as_view()),
+    path('courses/', views.CourseList.as_view(), name='courses'),
+    path('courses/<int:pk>/', views.CourseDetail.as_view(), name='courses-detail'),
     path('docs/', include_docs_urls(title='Courses API', public=False)),
 ]
